935eeafc28ad1b1d77cebaff81d0b7cae3279661
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 02:09:26 +0100

    Initial commit

---
+++ config.lua
@@ -0,0 +1,305 @@
+Config = {}
+Config.Locale = GetConvar('esx:locale', 'fr')
+
+Config.DrawDistance = 10.0
+
+Config.Markers = {
+	EntryPoint = {
+		Type = 21,
+		Size = {
+			x = 1.0, y = 1.0, z = 0.5,
+		},
+		Color = {
+			r = 50, g = 200, b = 50,
+		},
+	},
+	GetOutPoint = {
+		Type = 21,
+		Size = {
+			x = 1.0, y = 1.0, z = 0.5,
+		},
+		Color = {
+			r = 200, g = 51, b = 51,
+		},
+	},
+}
+
+Config.Garages = {
+	VespucciBoulevard = {
+		EntryPoint = {
+			x = -285.2, y = -886.5004, z = 31.07568,
+		},
+		SpawnPoint = {
+			x = -309.3, y = -897.0002, z = 31.07648, heading = 358.8335,
+		},
+		Sprite = 357, Scale = 0.7, Colour = 3,
+		ImpoundedName = "LosSantos",
+	},
+	SanAndreasAvenue = {
+		EntryPoint = {
+			x = 216.4, y = -786.6003, z = 30.81741,
+		},
+		SpawnPoint = {
+			x = 218.9469, y = -779.6987, z = 30.80582, heading = 335.9991,
+		},
+		Sprite = 357, Scale = 0.7, Colour = 3,
+		ImpoundedName = "LosSantos",
+	},
+	DiamondCasino = {
+		EntryPoint = {
+            x = 883.5903, y = -43.45465, z = 78.76418,
+        },
+        SpawnPoint = {
+            x = 901.46, y = -54.39, z = 78.76, heading = 145.87,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "LosSantos",
+    },				
+	ClintonAv = {
+        EntryPoint = {
+            x = 366.7566, y = 291.8114, z = 103.3966,
+        },
+        SpawnPoint = {
+            x = 373.7742, y = 270.1126, z = 103.0561, heading = 241.5558,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "LosSantos",
+    },		
+	GroveStreet = {
+        EntryPoint = {
+            x = -57.55466, y = -1836.019, z = 26.64645,
+        },
+        SpawnPoint = {
+            x = -48.87006, y = -1831.29, z = 26.51425, heading = 312.4902,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "LosSantos",
+    },
+	ElRanchoBvd = {
+        EntryPoint = {
+            x = 732.3476, y = -2018.513, z = 29.28265,
+        },
+        SpawnPoint = {
+            x = 744.3899, y = -2015.84, z = 29.28712, heading = 275.5775,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "LosSantos",
+    },
+	DelPiero = {
+        EntryPoint = {
+            x = -1573.825, y = -1035.855, z = 13.01797,
+        },
+        SpawnPoint = {
+            x = -1573.69, y = -1007.648, z = 13.01832, heading = 136.2437,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "LosSantos",
+    },
+	AmericanoWay = {
+        EntryPoint = {
+            x = -1666.101, y = 70.89622, z = 63.4337,
+        },
+        SpawnPoint = {
+            x = -1652.813, y = 52.28391, z = 62.58579, heading = 337.2132,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "LosSantos",
+    },
+	RockFordDrv = {
+        EntryPoint = {
+            x = -718.2157, y = -58.91401, z = 37.30375,
+        },
+        SpawnPoint = {
+            x = -744.1804, y = -64.89966, z = 41.7527, heading = 104.8672,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "LosSantos",
+    },
+	GalileoOBS = {
+        EntryPoint = {
+            x = -412.467, y = 1208.545, z = 325.6413,
+        },
+        SpawnPoint = {
+            x = -393.1299, y = 1202.64, z = 325.6408, heading = 169.4386,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "LosSantos",
+    },
+	EclipseBvd = {
+        EntryPoint = {
+            x = -336.5687, y = 274.4105, z = 85.8576,
+        },
+        SpawnPoint = {
+            x = -358.88, y = 264.0601, z = 84.73651, heading = 180.2324,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "LosSantos",
+    },
+	PkgBennys = {
+        EntryPoint = {
+            x = -184.87, y = -1291.18, z = 31.29587,
+        },
+        SpawnPoint = {
+            x = -174.862, y = -1302.532, z = 31.30996, heading = 90.63627,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "LosSantos",
+    },
+	ElRancho02 = {
+        EntryPoint = {
+            x = 1193.968, y = -1062.987, z = 41.70666,
+        },
+        SpawnPoint = {
+            x = 1206.82, y = -1077.9, z = 40.31368, heading = 117.0881,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "LosSantos",
+    },
+	LSAirport = {
+        EntryPoint = {
+            x = -835.9762, y = -2504.89, z = 13.83051,
+        },
+        SpawnPoint = {
+            x = -844.2302, y = -2490.009, z = 13.83063, heading = 194.9761,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "LosSantos",
+    },
+	LSHarbor = {
+        EntryPoint = {
+            x = 859.3379, y = -3132.832, z = 5.900806,
+        },
+        SpawnPoint = {
+            x = 848.8802, y = -3137.15, z = 5.900042, heading = 82.73539,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "LosSantos",
+    },
+	PkgPublicCayo = {
+        EntryPoint = {
+            x = 4505.845, y = -4475.57, z = 4.19535,
+        },
+        SpawnPoint = {
+            x = 4488.734, y = -4480.024, z = 4.211774, heading = 109.1134,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "LosSantos",
+    },
+	VillaCayo = {
+        EntryPoint = {
+            x = 5083.766, y = -5713.997, z = 15.7741,
+        },
+        SpawnPoint = {
+            x = 5071.494, y = -5702.49, z = 16.26362, heading = 56.39029,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "LosSantos",
+    },
+	ChumashRd1 = {
+        EntryPoint = {
+            x = -3145.584, y = 1093.527, z = 20.69526,
+        },
+        SpawnPoint = {
+            x = -3149.815, y = 1068.095, z = 20.64836, heading = 254.0647,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "LosSantos",
+    },
+	PkgPaleto01 = {
+        EntryPoint = {
+            x = -284.7251, y = 6053.587, z = 31.513,
+        },
+        SpawnPoint = {
+            x = -277.0299, y = 6064.72, z = 31.46983, heading = 37.72098,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "PaletoBay",
+    },
+	PkgPaleto02 = {
+        EntryPoint = {
+            x = 139.3571, y = 6595.147, z = 31.85067,
+        },
+        SpawnPoint = {
+            x = 156.8597, y = 6576.26, z = 31.82216, heading = 227.6128,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "PaletoBay",
+    },
+	PkGrapeseed = {
+        EntryPoint = {
+            x = 1710.246, y = 4804.347, z = 41.79272,
+        },
+        SpawnPoint = {
+            x = 1694.487, y = 4798.937, z = 41.86735, heading = 147.213,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "SandyShores",
+    },
+	PkgSandyShore = {
+        EntryPoint = {
+            x = 1564.548, y = 3793.576, z = 34.11957,
+        },
+        SpawnPoint = {
+            x = 1562.14, y = 3782.51, z = 34.42325, heading = 219.3226,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "SandyShores",
+    },
+	PkgSandyAirport = {
+        EntryPoint = {
+            x = 1755.422, y = 3288.66, z = 41.12197,
+        },
+        SpawnPoint = {
+            x = 1771.28, y = 3282.83, z = 41.4329, heading = 231.1956,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "SandyShores",
+    },
+	PkgPrison = {
+        EntryPoint = {
+            x = 1864.41, y = 2621.71, z = 45.65641,
+        },
+        SpawnPoint = {
+            x = 1881.197, y = 2627.268, z = 45.67262, heading = 181.2662,
+        },
+	    Sprite = 357, Scale = 0.7, Colour = 3,
+        ImpoundedName = "SandyShores",
+    },
+}
+
+Config.Impounds = {
+	LosSantos = {
+		GetOutPoint = {
+			x = 400.7, y = -1630.5, z = 29.3,
+		},
+		SpawnPoint = {
+			x = 401.9, y = -1647.4, z = 29.2, heading = 323.3,
+		},
+		Sprite = 524, Scale = 0.7, Colour = 1, Cost = 800,
+	},
+	PaletoBay = {
+		GetOutPoint = {
+			x = -211.4, y = 6206.5, z = 31.4,
+		},
+		SpawnPoint = {
+			x = -204.6, y = 6221.6, z = 30.5, heading = 227.2,
+		},
+		Sprite = 524, Scale = 0.7, Colour = 1, Cost = 800,
+	},
+	SandyShores = {
+		GetOutPoint = {
+			x = 1728.2, y = 3709.3, z = 33.2,
+		},
+		SpawnPoint = {
+			x = 1722.7, y = 3713.6, z = 33.2, heading = 19.9,
+		},
+		Sprite = 524, Scale = 0.7, Colour = 1, Cost = 800,
+	},
+}
+
+exports("getGarages", function()
+	return Config.Garages
+end)
+exports("getImpounds", function()
+	return Config.Impounds
+end)
